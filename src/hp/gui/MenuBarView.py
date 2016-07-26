19bdd3e6f967f04389f535bedc015b2cdc6b009e
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-07-26 18:33:35 -0400

    Added: Menu Bar modes for manual and mode

--- src/hp/gui/MenuBarView.py
+++ src/hp/gui/MenuBarView.py
@@ -4,8 +4,10 @@ class MenuBarView(wx.MenuBar):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.menuFile = self.initFileTab()
+        self.menuMode = self.initModeTab()
         self.menuHelp = self.initHelpTab()
         self.Append(self.menuFile, '&File')
+        self.Append(self.menuMode, '&Mode')
         self.Append(self.menuHelp, '&Help')
     def initFileTab(self):
         self.menuFile = wx.Menu()
@@ -30,5 +32,13 @@ class MenuBarView(wx.MenuBar):
         self.menuFile = menuFile
     def getExitItem(self):
         return self.exitItem
+    def initModeTab(self):
+        self.menuMode = wx.Menu()
+        self.manualItem = self.menuMode.Append(wx.ID_FILE1, 'Manual')
+        self.timerItem = self.menuMode.Append(wx.ID_FILE2, 'Timer')
+        return self.menuMode
+
+
+
 
 
