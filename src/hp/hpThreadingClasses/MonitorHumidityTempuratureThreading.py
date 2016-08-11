a15ff6f063c218bdc6f4a3e1acf119c5647c4636
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-11 19:34:53 -0400

    Testing: air/humidity monitoring

--- src/hp/hpThreadingClasses/MonitorHumidityTempuratureThreading.py
+++ src/hp/hpThreadingClasses/MonitorHumidityTempuratureThreading.py
@@ -1,16 +1,13 @@
 import threading
 import time
 from ThirdPartyAPIs.Adafruit_Python_DHT.examples.AdafruitDHT2 import AdafruitDHT2
-from ThirdPartyAPIs.MinipHBFW.BeagleBone.pHReader import pHReader
-import ThirdPartyAPIs.UnderwaterTemperatureReader.Temperature as Temperature
 from data.Monitor import Monitor
-class MonitorThreading(threading.Thread):
+class MonitorHumidityTemperatureThreading(threading.Thread):
     def __init__(self,monitorController):
-        super(MonitorThreading, self).__init__()
+        super(MonitorHumidityTemperatureThreading, self).__init__()
         self.monitorController = monitorController
         self.isDead = False
         self.initAirTemperature()
-       # self.initpHSensor()
 
 
     def run(self):
@@ -25,27 +22,17 @@ class MonitorThreading(threading.Thread):
                 self.monitorController.updateAirHumidityView()
                 time.sleep(1)
             except:
+                time.sleep(2)
                 continue
-
-
 
 
     def initAirTemperature(self):
         self.airTemperatureRightSideSensor = AdafruitDHT2('22', Monitor.RIGHTSIDEAIRSENSOR)
         self.airTemperatureLeftSideSensor = AdafruitDHT2('22', Monitor.LEFTSIDEAIRSENSOR)
-    def initpHSensor(self):
-        self.pHReader = pHReader()
-
-    def getWaterTemperature(self):
-        return Temperature.read_temp()
     def averageHumidityAndTemp(self,humidityR,humidityL,tempR,tempL):
         humidityAvg = (humidityR + humidityR)/2
+        humidityAvg = round(humidityAvg,2)
         tempAvg = (tempR + tempL)/2
         return humidityAvg, tempAvg
-
-
-
-
-
     def die(self):
         self.isDead = True
