449427f84b1264043233e05c77f19313c85e892f
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-11 14:44:10 -0400

    Testing: humidifier

--- src/hp/humidifer.py
+++ src/hp/humidifer.py
@@ -2,8 +2,7 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-print sys.path
-from src.hp.ThirdPartyAPIs.Adafruit_Python_DHT.examples import AdafruitDHT2
+import AdafruitDHT2
 #testing
 def main():
     GPIO.setmode(GPIO.BCM)
