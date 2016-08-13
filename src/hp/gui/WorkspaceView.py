06c9dac564dc6d1f3db587abd8940b8d2ac5c654
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-12 23:43:04 -0400

    Testing: Configuring Workspace

--- src/hp/gui/WorkspaceView.py
+++ src/hp/gui/WorkspaceView.py
@@ -1,4 +1,9 @@
 import wx
 
-class WorkspaceView(wx.Panel):
-    pass
+class WorkspaceView(wx.ScrollPanel):
+    def __init__(self, appGUI):
+        self.appGUI = appGUI
+        super(WorkspaceView, self).__init__(id.wx_ANY, name =u'workspace', parent = self.appGUI,
+                                            pos = wx.Point(0,50), size = wx.Size(950,800), style=wx.Tab_TRAVERSAL)
+        self.SetInitalSize(wx.Size(950,800))
+        self.SetBackgroundColour(wx.Colour(185, 242, 253))
