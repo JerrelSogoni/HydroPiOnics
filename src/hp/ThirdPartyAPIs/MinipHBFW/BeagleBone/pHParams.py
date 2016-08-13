3aaab9cc9548036ff61adfb6f859c71f0cbd20bb
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-12 23:29:55 -0400

    Monitor System Completed

--- src/hp/ThirdPartyAPIs/MinipHBFW/BeagleBone/pHParams.py
+++ src/hp/ThirdPartyAPIs/MinipHBFW/BeagleBone/pHParams.py
@@ -21,7 +21,7 @@ import os
 class pHParams(object):
     JSONFILE = os.path.realpath(__file__).replace("pHParams.py", "ph.json")
     if(JSONFILE[len(JSONFILE) - 1] == 'c'):
-        JSONFILE = JSONFILE[0:len(JSONFILE) - 2]
+        JSONFILE = JSONFILE[0:len(JSONFILE) - 1]
 
     def __init__(self, calibs=None):
         """Creates a new pHParams object and initializes its calibs dictionary
