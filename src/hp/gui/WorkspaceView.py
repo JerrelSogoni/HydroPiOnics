d1213881c8101d688e85ba48438352ced59bd3aa
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-12 23:51:56 -0400

    Correction: correced spelling for a method

--- src/hp/gui/WorkspaceView.py
+++ src/hp/gui/WorkspaceView.py
@@ -5,5 +5,5 @@ class WorkspaceView(wx.Panel):
         self.appGUI = appGUI
         super(WorkspaceView, self).__init__(id=wx.ID_ANY, name =u'workspace', parent = self.appGUI,
                                             pos = wx.Point(0,50), size = wx.Size(950,800), style=wx.TAB_TRAVERSAL)
-        self.SetInitalSize(wx.Size(950,800))
+        self.SetMaxSize(wx.Size(950,800))
         self.SetBackgroundColour(wx.Colour(185, 242, 253))
