2b5a1912e6afc2aa447b02490b2042a33f5cf7b7
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-13 12:19:15 -0400

    Added: Static text describing pictures

--- src/hp/HydroPiOnics.py
+++ src/hp/HydroPiOnics.py
@@ -52,14 +52,6 @@ class HydroPiOnics(wx.Frame):
 
 
 
-        # self.environmentControlText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSENVIRONMENTCONTROLTEXT,
-        #       label=u'Environmental Control', name=u'environmentControlText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(464, 8),
-        #       size=wx.Size(154, 16), style=0)
-        # self.environmentControlText.SetLabelText(u'Environmental Control')
-        # self.environmentControlText.SetFont(wx.Font(9, wx.SCRIPT, wx.NORMAL,
-        #       wx.NORMAL, False, u'Lucida Calligraphy'))
-        #
 
 
         # self.ventFanCheckBox = wx.CheckBox(id=wxID_HYDROPIONICSVENTFANCHECKBOX,
@@ -95,64 +87,9 @@ class HydroPiOnics(wx.Frame):
         #       size=wx.Size(78, 15), style=0)
         # self.airHeaterCheckBox.SetValue(True)
 
-        # self.intakeFanText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSINTAKEFANTEXT,
-        #       label=u'Intake Fan', name=u'intakeFanText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(400, 216),
-        #       size=wx.Size(69, 15), style=0)
-        # self.intakeFanText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        #
-        # self.ventFanText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSVENTFANTEXT,
-        #       label=u'Vent Fan', name=u'ventFanText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(400, 120),
-        #       size=wx.Size(56, 15), style=0)
-        # self.ventFanText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL, wx.NORMAL,
-        #       False, u'Showcard Gothic'))
-        #
-        # self.waterPumpAirTitle = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSWATERPUMPAIRTITLE,
-        #       label=u'Water/Air Pump', name=u'waterPumpAirTitle',
-        #       parent=self.scrolledPanel1, pos=wx.Point(8, 32), size=wx.Size(104,
-        #       15), style=0)
-        # self.waterPumpAirTitle.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        # self.waterPumpAirTitle.SetToolTipString(u'')
-        #
-        # self.exhaustFanText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSEXHAUSTFANTEXT,
-        #       label=u'Exhaust Fan', name=u'exhaustFanText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(392, 24),
-        #       size=wx.Size(80, 15), style=0)
-        # self.exhaustFanText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        #
-        # self.humidifierText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSHUMIDIFIERTEXT,
-        #       label=u'Humifier', name=u'humidifierText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(400, 400),
-        #       size=wx.Size(61, 15), style=0)
-        # self.humidifierText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        #
-        # self.airHeaterText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSAIRHEATERTEXT,
-        #       label=u'Air Heater', name=u'airHeaterText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(400, 496),
-        #       size=wx.Size(68, 15), style=0)
-        # self.airHeaterText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        #
-        # self.waterControlText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSWATERCONTROLTEXT,
-        #       label=u'Water Control', name=u'waterControlText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(24, 8), size=wx.Size(100,
-        #       16), style=0)
-        # self.waterControlText.SetFont(wx.Font(9, wx.SCRIPT, wx.NORMAL,
-        #       wx.NORMAL, False, u'Lucida Handwriting'))
-        # self.waterControlText.SetToolTipString(u'')
-        #
-        # self.airFilterFanText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSAIRFILTERFANTEXT,
-        #       label=u'Air Filter Fan', name=u'airFilterFanText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(384, 296),
-        #       size=wx.Size(88, 15), style=0)
-        # self.airFilterFanText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        #
+
+
+
         # self.airFilterFanCheckBox = wx.CheckBox(id=wxID_HYDROPIONICSAIRFILTERFANCHECKBOX,
         #       label=u'On/Off', name=u'airFilterFanCheckBox',
         #       parent=self.scrolledPanel1, pos=wx.Point(472, 336),
@@ -171,30 +108,7 @@ class HydroPiOnics(wx.Frame):
         #       pos=wx.Point(472, 608), size=wx.Size(78, 15), style=0)
         # self.ledCheckBox.SetValue(False)
         #
-        # self.mixToPlantText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSMIXTOPLANTTEXT,
-        #       label=u'Mix to Plant', name=u'mixToPlantText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(16, 112),
-        #       size=wx.Size(82, 15), style=0)
-        # self.mixToPlantText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        # self.mixToPlantText.SetToolTipString(u'')
-        #
-        # self.plantToMixText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSPLANTTOMIXTEXT,
-        #       label=u'Plant to Mix', name=u'plantToMixText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(16, 216),
-        #       size=wx.Size(82, 15), style=0)
-        # self.plantToMixText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        # self.plantToMixText.SetToolTipString(u'')
-        #
-        # self.mixToDrainText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSMIXTODRAINTEXT,
-        #       label=u'Mix to Drain', name=u'mixToDrainText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(16, 320),
-        #       size=wx.Size(84, 15), style=0)
-        # self.mixToDrainText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.NORMAL, False, u'Showcard Gothic'))
-        # self.mixToDrainText.SetToolTipString(u'')
-        #
+
 
         # self.mixToPlantCheckBox = wx.CheckBox(id=wxID_HYDROPIONICSMIXTOPLANTCHECKBOX,
         #       label=u'On/Off', name=u'mixToPlantCheckBox',
@@ -223,6 +137,8 @@ class HydroPiOnics(wx.Frame):
         #       self.OnMixToDrainCheckBoxCheckbox,
         #       id=wxID_HYDROPIONICSMIXTODRAINCHECKBOX)
         #
+        ##### Temperature Range and PH level Range Will not be implemented Yet since I cannot control them
+        ## Incorporating in future upgrades
         # self.temperatureRangeText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSTEMPERATURERANGETEXT,
         #       label=u'Temperature Range', name=u'temperatureRangeText',
         #       parent=self.scrolledPanel1, pos=wx.Point(32, 432),
@@ -272,50 +188,6 @@ class HydroPiOnics(wx.Frame):
         # self.temperatureRangeEndSlider.Bind(wx.EVT_COMMAND_SCROLL,
         #       self.OnTemperatureRangeEndSliderCommandScroll,
         #       id=wxID_HYDROPIONICSTEMPERATURERANGEENDSLIDER)
-        #
-        # self.humdiityRangeText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSHUMDIITYRANGETEXT,
-        #       label=u'Humidity Range', name=u'humdiityRangeText',
-        #       parent=self.scrolledPanel1, pos=wx.Point(32, 528),
-        #       size=wx.Size(86, 15), style=0)
-        #
-        # self.humidityRangeStartValue = wx.lib.masked.textctrl.TextCtrl(id=wxID_HYDROPIONICSHUMIDITYRANGESTARTVALUE,
-        #       name=u'humidityRangeStartValue', parent=self.scrolledPanel1,
-        #       pos=wx.Point(144, 512), size=wx.Size(40, 32), style=0, value='')
-        # self.humidityRangeStartValue.Bind(wx.EVT_TEXT_ENTER,
-        #       self.OnHumidityRangeStartValueTextEnter,
-        #       id=wxID_HYDROPIONICSHUMIDITYRANGESTARTVALUE)
-        #
-        # self.genStaticText21 = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSGENSTATICTEXT21,
-        #       label=u'-', name='genStaticText21', parent=self.scrolledPanel1,
-        #       pos=wx.Point(216, 520), size=wx.Size(6, 15), style=0)
-        # self.genStaticText21.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
-        #       wx.BOLD, False, u'Showcard Gothic'))
-        # self.genStaticText21.SetBackgroundColour(wx.Colour(143, 210, 250))
-        #
-        # self.humidityRangeEndValue = wx.lib.masked.textctrl.TextCtrl(id=wxID_HYDROPIONICSHUMIDITYRANGEENDVALUE,
-        #       name=u'humidityRangeEndValue', parent=self.scrolledPanel1,
-        #       pos=wx.Point(240, 512), size=wx.Size(40, 32), style=0, value='')
-        # self.humidityRangeEndValue.Bind(wx.EVT_TEXT_ENTER,
-        #       self.OnHumidityRangeEndValueTextEnter,
-        #       id=wxID_HYDROPIONICSHUMIDITYRANGEENDVALUE)
-        #
-        # self.humidityRangeStartSlider = wx.Slider(id=wxID_HYDROPIONICSHUMIDITYRANGESTARTSLIDER,
-        #       maxValue=100, minValue=0, name=u'humidityRangeStartSlider',
-        #       parent=self.scrolledPanel1, pos=wx.Point(16, 560),
-        #       size=wx.Size(360, 32), style=wx.SL_HORIZONTAL, value=0)
-        # self.humidityRangeStartSlider.SetPageSize(1)
-        # self.humidityRangeStartSlider.Bind(wx.EVT_COMMAND_SCROLL,
-        #       self.OnHumidityRangeStartSliderCommandScroll,
-        #       id=wxID_HYDROPIONICSHUMIDITYRANGESTARTSLIDER)
-        #
-        # self.humidityRangeEndSlider = wx.Slider(id=wxID_HYDROPIONICSHUMIDITYRANGEENDSLIDER,
-        #       maxValue=100, minValue=0, name=u'humidityRangeEndSlider',
-        #       parent=self.scrolledPanel1, pos=wx.Point(16, 600),
-        #       size=wx.Size(360, 24), style=wx.SL_HORIZONTAL, value=0)
-        # self.humidityRangeEndSlider.SetPageSize(1)
-        # self.humidityRangeEndSlider.Bind(wx.EVT_COMMAND_SCROLL,
-        #       self.OnHumidityRangeEndSliderCommandScroll,
-        #       id=wxID_HYDROPIONICSHUMIDITYRANGEENDSLIDER)
         #
         # self.pHRangeText = wx.lib.stattext.GenStaticText(ID=wxID_HYDROPIONICSPHRANGETEXT,
         #       label=u'PH Level Range', name=u'pHRangeText',
--- src/hp/gui/WorkspaceView.py
+++ src/hp/gui/WorkspaceView.py
@@ -79,3 +79,146 @@ class WorkspaceView(wx.Panel):
                name=u'mixToDrainPicture', parent=self,
                pos=wx.Point(32, 336), size=wx.Size(50, 66), style=0)
 
+    def initStaticText(self):
+
+        self.environmentControlText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+               label=u'Environmental Control', name=u'environmentControlText',
+               parent=self, pos=wx.Point(464, 8),
+               size=wx.Size(154, 16), style=0)
+        self.environmentControlText.SetLabelText(u'Environmental Control')
+        self.environmentControlText.SetFont(wx.Font(9, wx.SCRIPT, wx.NORMAL,
+               wx.NORMAL, False, u'Lucida Calligraphy'))
+
+        self.intakeFanText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+               label=u'Intake Fan', name=u'intakeFanText',
+               parent=self, pos=wx.Point(400, 216),
+               size=wx.Size(69, 15), style=0)
+        self.intakeFanText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+               wx.NORMAL, False, u'Showcard Gothic'))
+
+        self.ventFanText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+               label=u'Vent Fan', name=u'ventFanText',
+               parent=self, pos=wx.Point(400, 120),
+               size=wx.Size(56, 15), style=0)
+        self.ventFanText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL, wx.NORMAL,
+               False, u'Showcard Gothic'))
+        self.waterPumpAirTitle = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Water/Air Pump', name=u'waterPumpAirTitle',
+              parent=self, pos=wx.Point(8, 32), size=wx.Size(104,
+              15), style=0)
+        self.waterPumpAirTitle.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.NORMAL, False, u'Showcard Gothic'))
+        self.waterPumpAirTitle.SetToolTipString(u'')
+
+        self.exhaustFanText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Exhaust Fan', name=u'exhaustFanText',
+              parent=self, pos=wx.Point(392, 24),
+              size=wx.Size(80, 15), style=0)
+        self.exhaustFanText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.NORMAL, False, u'Showcard Gothic'))
+
+        self.humidifierText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Humifier', name=u'humidifierText',
+              parent=self, pos=wx.Point(400, 400),
+              size=wx.Size(61, 15), style=0)
+        self.humidifierText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.NORMAL, False, u'Showcard Gothic'))
+
+        self.airHeaterText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Air Heater', name=u'airHeaterText',
+              parent=self, pos=wx.Point(400, 496),
+              size=wx.Size(68, 15), style=0)
+        self.airHeaterText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.NORMAL, False, u'Showcard Gothic'))
+
+        self.waterControlText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Water Control', name=u'waterControlText',
+              parent=self, pos=wx.Point(24, 8), size=wx.Size(100,
+              16), style=0)
+        self.waterControlText.SetFont(wx.Font(9, wx.SCRIPT, wx.NORMAL,
+              wx.NORMAL, False, u'Lucida Handwriting'))
+        self.waterControlText.SetToolTipString(u'')
+
+        self.airFilterFanText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Air Filter Fan', name=u'airFilterFanText',
+              parent=self, pos=wx.Point(384, 296),
+              size=wx.Size(88, 15), style=0)
+        self.airFilterFanText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.NORMAL, False, u'Showcard Gothic'))
+
+        self.mixToPlantText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Mix to Plant', name=u'mixToPlantText',
+              parent=self, pos=wx.Point(16, 112),
+              size=wx.Size(82, 15), style=0)
+        self.mixToPlantText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.NORMAL, False, u'Showcard Gothic'))
+        self.mixToPlantText.SetToolTipString(u'')
+
+        self.plantToMixText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Plant to Mix', name=u'plantToMixText',
+              parent=self, pos=wx.Point(16, 216),
+              size=wx.Size(82, 15), style=0)
+        self.plantToMixText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.NORMAL, False, u'Showcard Gothic'))
+        self.plantToMixText.SetToolTipString(u'')
+
+        self.mixToDrainText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Mix to Drain', name=u'mixToDrainText',
+              parent=self, pos=wx.Point(16, 320),
+              size=wx.Size(84, 15), style=0)
+        self.mixToDrainText.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.NORMAL, False, u'Showcard Gothic'))
+        self.mixToDrainText.SetToolTipString(u'')
+
+        self.temperatureRangeText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Temperature Range', name=u'temperatureRangeText',
+              parent=self, pos=wx.Point(32, 432),
+              size=wx.Size(104, 15), style=0)
+        self.temperatureRangeText.SetToolTipString(u'in Fahrenheit')
+        self.temperatureRangeText.SetHelpText(u'in Fahrenheit')
+
+        self.humdiityRangeText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'Humidity Range', name=u'humdiityRangeText',
+              parent=self, pos=wx.Point(32, 528),
+              size=wx.Size(86, 15), style=0)
+
+        self.humidityRangeStartValue = wx.lib.masked.textctrl.TextCtrl(id=wx.ID_ANY,
+              name=u'humidityRangeStartValue', parent=self,
+              pos=wx.Point(144, 512), size=wx.Size(40, 32), style=0, value='')
+        # #self.humidityRangeStartValue.Bind(wx.EVT_TEXT_ENTER,
+        #       self.OnHumidityRangeStartValueTextEnter,
+        #       id=wxID_HYDROPIONICSHUMIDITYRANGESTARTVALUE)
+
+        self.genStaticText21 = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
+              label=u'-', name='genStaticText21', parent=self,
+              pos=wx.Point(216, 520), size=wx.Size(6, 15), style=0)
+        self.genStaticText21.SetFont(wx.Font(9, wx.DECORATIVE, wx.NORMAL,
+              wx.BOLD, False, u'Showcard Gothic'))
+        self.genStaticText21.SetBackgroundColour(wx.Colour(143, 210, 250))
+
+        self.humidityRangeEndValue = wx.lib.masked.textctrl.TextCtrl(id=wx.ID_ANY,
+              name=u'humidityRangeEndValue', parent=self,
+              pos=wx.Point(240, 512), size=wx.Size(40, 32), style=0, value='')
+        # self.humidityRangeEndValue.Bind(wx.EVT_TEXT_ENTER,
+        #       self.OnHumidityRangeEndValueTextEnter,
+        #       id=wxID_HYDROPIONICSHUMIDITYRANGEENDVALUE)
+
+        self.humidityRangeStartSlider = wx.Slider(id=wx.ID_ANY,
+              maxValue=100, minValue=0, name=u'humidityRangeStartSlider',
+              parent=self, pos=wx.Point(16, 560),
+              size=wx.Size(360, 32), style=wx.SL_HORIZONTAL, value=0)
+        self.humidityRangeStartSlider.SetPageSize(1)
+        # self.humidityRangeStartSlider.Bind(wx.EVT_COMMAND_SCROLL,
+        #       self.OnHumidityRangeStartSliderCommandScroll,
+        #       id=wxID_HYDROPIONICSHUMIDITYRANGESTARTSLIDER)
+
+        self.humidityRangeEndSlider = wx.Slider(id=wx.ID_ANY,
+              maxValue=100, minValue=0, name=u'humidityRangeEndSlider',
+              parent=self, pos=wx.Point(16, 600),
+              size=wx.Size(360, 24), style=wx.SL_HORIZONTAL, value=0)
+        self.humidityRangeEndSlider.SetPageSize(1)
+        # self.humidityRangeEndSlider.Bind(wx.EVT_COMMAND_SCROLL,
+        #       self.OnHumidityRangeEndSliderCommandScroll,
+        #       id=wxID_HYDROPIONICSHUMIDITYRANGEENDSLIDER)
+
+
