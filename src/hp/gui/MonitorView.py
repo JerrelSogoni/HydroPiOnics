198981b1e31c23f8cf84ba41f6cee934e5fa701e
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-15 14:47:00 -0400

    Testing: Motor MVC, attempting to get workspace IMG_LOCATION constant

--- src/hp/gui/MonitorView.py
+++ src/hp/gui/MonitorView.py
@@ -26,7 +26,7 @@ class MonitorView(wx.Panel):
         self.AirTemp.SetFont(wx.Font(13, wx.ROMAN, wx.NORMAL, wx.NORMAL, False,
                 u'Modern No. 20'))
         self.TemperatureSensor = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'', name=u'TemperatureSensor', parent=self,
+               label=u'', name=u'TemperatureSensor', parent=self.workspace,
                pos=wx.Point(140, 15), size=wx.Size(14, 16), style=0)
         self.TemperatureSensor.SetInitialSize(wx.Size(65, 15))
         self.TemperatureSensor.SetSizeHints(-1, -1, -1, -1)
@@ -35,12 +35,12 @@ class MonitorView(wx.Panel):
         self.TemperatureSensor.SetBackgroundStyle(wx.BG_STYLE_COLOUR)
     def initHumidityMonitor(self):
         self.humidityText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'Humidity:', name=u'humidityText', parent=self,
+               label=u'Humidity:', name=u'humidityText', parent=self.workspace,
                pos=wx.Point(235, 15), size=wx.Size(56, 16), style=0)
         self.humidityText.SetFont(wx.Font(13, wx.ROMAN, wx.NORMAL, wx.NORMAL,
                False, u'Modern No. 20'))
         self.humiditySensor = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'', name=u'humiditySensor', parent=self,
+               label=u'', name=u'humiditySensor', parent=self.workspace,
                pos=wx.Point(317, 15), size=wx.Size(56, 15), style=0)
         self.humiditySensor.SetFont(wx.Font(13, wx.SCRIPT, wx.NORMAL, wx.BOLD,
                False, u'Lucida Handwriting'))
@@ -48,25 +48,25 @@ class MonitorView(wx.Panel):
     def initWaterTempMonitor(self):
         self.waterTemperatureText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
                label=u'Water Temperature:', name=u'waterTemperatureText',
-               parent=self, pos=wx.Point(420, 15), size=wx.Size(105,
+               parent=self.workspace, pos=wx.Point(420, 15), size=wx.Size(105,
                16), style=0)
         self.waterTemperatureText.SetFont(wx.Font(13, wx.ROMAN, wx.NORMAL,
                wx.NORMAL, False, u'Modern No. 20'))
 
         self.waterTemperatureSensor = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'', name=u'waterTemperatureSensor', parent=self,
+               label=u'', name=u'waterTemperatureSensor', parent=self.workspace,
                pos=wx.Point(587, 15), size=wx.Size(65, 15), style=0)
         self.waterTemperatureSensor.SetInitialSize(wx.Size(65, 15))
         self.waterTemperatureSensor.SetFont(wx.Font(13, wx.SCRIPT, wx.NORMAL,
                                             wx.BOLD, False, u'Lucida Handwriting'))
     def initPHMonitor(self):
         self.pHText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'PH Level:', name=u'pHText', parent=self,
+               label=u'PH Level:', name=u'pHText', parent=self.workspace,
                pos=wx.Point(680, 15), size=wx.Size(51, 15), style=0)
         self.pHText.SetFont(wx.Font(13, wx.ROMAN, wx.NORMAL, wx.NORMAL, False,
                u'Modern No. 20'))
         self.pHSensor = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'', name=u'pHSensor', parent=self,
+               label=u'', name=u'pHSensor', parent=self.workspace,
                pos=wx.Point(757, 15), size=wx.Size(30, 15), style=0)
         self.pHSensor.SetInitialSize(wx.Size(30, 15))
         self.pHSensor.SetFont(wx.Font(13, wx.SCRIPT, wx.NORMAL, wx.BOLD, False,
--- src/hp/gui/WorkspaceView.py
+++ src/hp/gui/WorkspaceView.py
@@ -29,7 +29,7 @@ class WorkspaceView(wx.Panel):
     def initWorkspaceCheckboxes(self):
         self.delayStartCheckBox = wx.CheckBox(id=wx.ID_ANY,
               label=u'Delay Start', name=u'delayStartCheckBox',
-              parent=self.workspace, pos=wx.Point(408, 648),
+              parent=self, pos=wx.Point(408, 648),
               size=wx.Size(78, 23), style=0)
         self.delayStartCheckBox.SetValue(False)
 
@@ -37,15 +37,14 @@ class WorkspaceView(wx.Panel):
 
 
     def initWorkspaceStaticText(self):
-        self.systemStatusText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-              label=u'System Status:', name=u'systemStatusText',
-              parent=self.workspace, pos=wx.Point(784, 688),
+        self.systemStatusText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,label=u'System Status:', name=u'systemStatusText',
+              parent=self, pos=wx.Point(784, 688),
               size=wx.Size(78, 13), style=0)
         self.systemStatusText.SetFont(wx.Font(9, wx.SWISS, wx.NORMAL, wx.NORMAL,
               False, u'Small Fonts'))
 
         self.systemStatus = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-              label=u'', name=u'systemStatus', parent=self.workspace,
+              label=u'', name=u'systemStatus', parent=self,
               pos=wx.Point(872, 688), size=wx.Size(12, 13), style=0)
         self.systemStatus.SetFont(wx.Font(9, wx.SWISS, wx.NORMAL, wx.NORMAL,
               False, u'Small Fonts'))
@@ -54,7 +53,7 @@ class WorkspaceView(wx.Panel):
         self.delayStartTime = wx.lib.masked.timectrl.TimeCtrl(display_seconds=True,
               fmt24hr=False, id=wx.ID_ANY,
               name=u'delayStartTime', oob_color=wx.NamedColour('Yellow'),
-              parent=self.workspace, pos=wx.Point(488, 648),
+              parent=self, pos=wx.Point(488, 648),
               size=wx.Size(85, 23), style=0, useFixedWidthFont=True,
               value='12:00:00 AM')
 
