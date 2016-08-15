2e9b374b14f7e94b7b10b63a61a07f05c6b11349
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-15 15:13:15 -0400

    Testing: Motor MVC, attempting to get workspace IMG_LOCATION constant

--- src/hp/gui/MonitorView.py
+++ src/hp/gui/MonitorView.py
@@ -25,7 +25,7 @@ class MonitorView(wx.Panel):
         self.AirTemp.SetFont(wx.Font(13, wx.ROMAN, wx.NORMAL, wx.NORMAL, False,
                 u'Modern No. 20'))
         self.TemperatureSensor = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'', name=u'TemperatureSensor', parent=self.workspace,
+               label=u'', name=u'TemperatureSensor', parent=self,
                pos=wx.Point(140, 15), size=wx.Size(14, 16), style=0)
         self.TemperatureSensor.SetInitialSize(wx.Size(65, 15))
         self.TemperatureSensor.SetSizeHints(-1, -1, -1, -1)
@@ -34,12 +34,12 @@ class MonitorView(wx.Panel):
         self.TemperatureSensor.SetBackgroundStyle(wx.BG_STYLE_COLOUR)
     def initHumidityMonitor(self):
         self.humidityText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'Humidity:', name=u'humidityText', parent=self.workspace,
+               label=u'Humidity:', name=u'humidityText', parent=self,
                pos=wx.Point(235, 15), size=wx.Size(56, 16), style=0)
         self.humidityText.SetFont(wx.Font(13, wx.ROMAN, wx.NORMAL, wx.NORMAL,
                False, u'Modern No. 20'))
         self.humiditySensor = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'', name=u'humiditySensor', parent=self.workspace,
+               label=u'', name=u'humiditySensor', parent=self,
                pos=wx.Point(317, 15), size=wx.Size(56, 15), style=0)
         self.humiditySensor.SetFont(wx.Font(13, wx.SCRIPT, wx.NORMAL, wx.BOLD,
                False, u'Lucida Handwriting'))
@@ -47,25 +47,25 @@ class MonitorView(wx.Panel):
     def initWaterTempMonitor(self):
         self.waterTemperatureText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
                label=u'Water Temperature:', name=u'waterTemperatureText',
-               parent=self.workspace, pos=wx.Point(420, 15), size=wx.Size(105,
+               parent=self, pos=wx.Point(420, 15), size=wx.Size(105,
                16), style=0)
         self.waterTemperatureText.SetFont(wx.Font(13, wx.ROMAN, wx.NORMAL,
                wx.NORMAL, False, u'Modern No. 20'))
 
         self.waterTemperatureSensor = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'', name=u'waterTemperatureSensor', parent=self.workspace,
+               label=u'', name=u'waterTemperatureSensor', parent=self,
                pos=wx.Point(587, 15), size=wx.Size(65, 15), style=0)
         self.waterTemperatureSensor.SetInitialSize(wx.Size(65, 15))
         self.waterTemperatureSensor.SetFont(wx.Font(13, wx.SCRIPT, wx.NORMAL,
                                             wx.BOLD, False, u'Lucida Handwriting'))
     def initPHMonitor(self):
         self.pHText = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'PH Level:', name=u'pHText', parent=self.workspace,
+               label=u'PH Level:', name=u'pHText', parent=self,
                pos=wx.Point(680, 15), size=wx.Size(51, 15), style=0)
         self.pHText.SetFont(wx.Font(13, wx.ROMAN, wx.NORMAL, wx.NORMAL, False,
                u'Modern No. 20'))
         self.pHSensor = wx.lib.stattext.GenStaticText(ID=wx.ID_ANY,
-               label=u'', name=u'pHSensor', parent=self.workspace,
+               label=u'', name=u'pHSensor', parent=self,
                pos=wx.Point(757, 15), size=wx.Size(30, 15), style=0)
         self.pHSensor.SetInitialSize(wx.Size(30, 15))
         self.pHSensor.SetFont(wx.Font(13, wx.SCRIPT, wx.NORMAL, wx.BOLD, False,
