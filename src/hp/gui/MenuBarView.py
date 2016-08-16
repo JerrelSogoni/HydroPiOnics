880810689d08bbe92a65714b1b411e9197db5d3e
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-15 21:37:14 -0400

    added: Front End finished, execpt for the about me page

--- src/hp/gui/MenuBarView.py
+++ src/hp/gui/MenuBarView.py
@@ -6,9 +6,12 @@ class MenuBarView(wx.MenuBar):
         self.menuFile = self.initFileTab()
         self.menuMode = self.initModeTab()
         self.menuHelp = self.initHelpTab()
+        self.menuRun = self.initRunTab()
         self.Append(self.menuFile, '&File')
         self.Append(self.menuMode, '&Mode')
+        self.Append(self.menuRun, '&Run')
         self.Append(self.menuHelp, '&Help')
+
     def initFileTab(self):
         self.menuFile = wx.Menu()
         self.openItem = self.menuFile.Append(wx.ID_OPEN, 'Open')
@@ -38,6 +41,11 @@ class MenuBarView(wx.MenuBar):
         self.timerItem = self.menuMode.AppendRadioItem(wx.ID_FILE2, "TIMER")
         self.environmentalItem = self.menuMode.AppendRadioItem(wx.ID_FILE3, "ENVIRONMENTAL")
         return self.menuMode
+    def initRunTab(self):
+        self.runMode = wx.Menu()
+        self.start = self.menuMode.AppendRadioItem(wx.ID_FILE4, "START")
+        self.stop = self.menuMode.AppendRadioItem(wx.ID_FILE5, "STOP")
+        return self.runMode
     def getMenuMode(self):
         return self.menuMode
     def getManualItem(self):
