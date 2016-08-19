da1cb336840632804ca753995df1bebe9df960ea
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-18 22:50:50 -0400

    Testing: electronic relay listeners

--- src/hp/hpThreadingClasses/ElectronicThreading.py
+++ src/hp/hpThreadingClasses/ElectronicThreading.py
@@ -21,58 +21,58 @@ class ElectronicThreading(threading.Thread):
     def run(self):
 
         try:
-            # GPIO.setup(self.pin, GPIO.OUT)
-            # GPIO.output(self.pin, GPIO.HIGH)
+            GPIO.setup(self.pin, GPIO.OUT)
+            GPIO.output(self.pin, GPIO.HIGH)
             if(self.mode == appData.TIMER):
                 while(not self.isDead):
                     if(self.cycleOn != 0):
-                        # GPIO.output(self.pin, GPIO.LOW)
+                        GPIO.output(self.pin, GPIO.LOW)
                         print  str(self.pin) +" On for " + str(self.cycleOn)
                         time.sleep(self.cycleOn)
                         print str(self.pin) +" Off for " + str(self.cycleOff)
-                        # GPIO.output(self.pin, GPIO.HIGH)
+                        GPIO.output(self.pin, GPIO.HIGH)
                         time.sleep(self.cycleOff)
             elif(self.mode == appData.ENVIRONMENTAL):
                 if(self.pin == ElectronicRelayEnvironment.WATERHEATERPIN):
                     while(not self.isDead):
                         if (monitor.waterTemperature < self.RangeLow):
                             if(not self.isOn):
-                                # GPIO.output(self.pin, GPIO.LOW)
+                                GPIO.output(self.pin, GPIO.LOW)
                                 self.isOn = True
                         elif (monitor.waterTemperature > self.RangeHigh):
                             print "Cannot really do much We dont have a water cooler"
-                            # GPIO.output(self.pin, GPIO.HIGH)
+                            GPIO.output(self.pin, GPIO.HIGH)
                             self.isOn = False
 
                         else:
-                            # GPIO.output(self.pin, GPIO.HIGH)
+                            GPIO.output(self.pin, GPIO.HIGH)
                             self.isOn = False
                 elif(self.pin == ElectronicRelayEnvironment.AIRHEATERPIN):
                     while (not self.isDead):
                         if (monitor.temperature < self.RangeLow):
                             if (not self.isOn):
-                                # GPIO.output(self.pin, GPIO.LOW)
+                                GPIO.output(self.pin, GPIO.LOW)
                                 self.isOn = True
                             else:
                                 continue
                         else:
-                            # GPIO.output(self.pin, GPIO.HIGH)
+                            GPIO.output(self.pin, GPIO.HIGH)
                             self.isOn = False
                 elif(self.pin == ElectronicRelayEnvironment.HUMIDIFIERPIN):
                     while (not self.isDead):
                         if (monitor.humidity < self.RangeLow):
                             if (not self.isOn):
-                                # GPIO.output(self.pin, GPIO.LOW)
+                                GPIO.output(self.pin, GPIO.LOW)
                                 self.isOn = True
                             else:
                                 continue
                         else:
-                            # GPIO.output(self.pin, GPIO.HIGH)
+                            GPIO.output(self.pin, GPIO.HIGH)
                             self.isOn = False
 
 
             else:
-                  # GPIO.output(self.pin, GPIO.LOW)
+                  GPIO.output(self.pin, GPIO.LOW)
                   while (not self.isDead):
                         continue
         except:
@@ -82,7 +82,7 @@ class ElectronicThreading(threading.Thread):
     def die(self):
         print str(self.pin) + " is Off"
         self.isDead = True
-        # GPIO.output(self.pin, GPIO.HIGH)
+        GPIO.output(self.pin, GPIO.HIGH)
 
     def changeRangeLow(self, air):
         self.airLow = air
