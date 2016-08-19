6c77ec7061bff27009b702d45683798a1374d410
JerrelSogoni <jerrel.sogoni@stonybrook.edu>
2016-08-19 12:17:55 -0400

    Testing: slider listeners

--- src/hp/hpThreadingClasses/MotorFanThreading.py
+++ src/hp/hpThreadingClasses/MotorFanThreading.py
@@ -2,13 +2,17 @@ import threading
 import time
 from ThirdPartyAPIs.Adafruit_Motor_HAT_Python_Library.Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
+global CycleOn
+global CycleOff
 class MotorFanThreading(threading.Thread):
     def __init__(self, motor, cycleOn = None, cycleOff = None, cycle = False):
         super(MotorFanThreading, self).__init__()
+        global CycleOn
+        global CycleOff
         self.isDead = False
         self.motor = motor
-        self.cycleOn = cycleOn
-        self.cycleOff = cycleOff
+        CycleOn = cycleOn
+        CycleOff = cycleOff
         self.cycle = cycle
         self.start()
 
@@ -19,9 +23,9 @@ class MotorFanThreading(threading.Thread):
                 if(self.cycleOn != 0):
                     self.motor.run(Adafruit_MotorHAT.FORWARD)
                     self.motor.setSpeed(255)
-                    time.sleep(self.cycleOn)
+                    time.sleep(CycleOn)
                     self.motor.run(Adafruit_MotorHAT.RELEASE)
-                    time.sleep(self.cycleOff)
+                    time.sleep(CycleOff)
                     continue
                 time.sleep(2147483647)
                 continue
@@ -37,9 +41,11 @@ class MotorFanThreading(threading.Thread):
         self.isDead = True
         self.motor.run(Adafruit_MotorHAT.RELEASE)
     def changeCycleOn(self, cycleOn):
-        self.cycleOn = cycleOn
+        global CycleOn
+        CycleOn = cycleOn
     def changeCycleOff(self, cycleOff):
-        self.cycleOff = cycleOff
+        global CycleOff
+        CycleOff = cycleOff
 
 
 
